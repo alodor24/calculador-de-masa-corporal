2c06d8a47ba9b0d9a26fa1eba0da6da2040e6780
alodor <jose-a-mendez@hotmail.com>
2019-09-22 17:29:13 -0400

    Integración de funcionalidad con Vue js

--- function.js
+++ function.js
@@ -1,17 +1,18 @@
-$(document).ready(() => {
-    
-    function calcular() {
-        let peso = parseInt($('#peso').val())
-        let estatura = parseInt($('#estatura').val())
-        estatura = (estatura * estatura) / 10000
-        
-        let resultado = peso / estatura
-        return resultado.toFixed(1)
+new Vue({
+    el  : '#app',
+    data: {
+        peso    : '',
+        estatura: ''
+    },
+    computed: {
+        calcular: function() {
+            let peso        = parseInt(this.peso)
+            let estatura    = parseInt(this.estatura)
+            
+            estatura        = (estatura * estatura) / 10000
+            
+            let resultado   = peso / estatura
+            return resultado.toFixed(1)
+        }
     }
-    
-    $('#calcular').click(() => {
-        let resultado = calcular()
-        $('#resultado').text(resultado)
-    })
-    
 })
